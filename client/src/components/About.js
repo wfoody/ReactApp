06cc38b13261947abfabdefd76aef1eb4ae666b9
wfoody <foodyw@gmail.com>
2020-12-16 10:28:04 -0500

    positioning footers

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -17,7 +17,7 @@ function About() {
                 <p className='aboutContainer'><i><b>REPRESENT</b></i>&nbsp; is a platform designed to connect you with your elected representatives. Using the <a href='https://developers.google.com/civic-information' target="_blank" rel="noopener noreferrer">Google Civic Information API</a> with any U.S. residential address, you have the ability to find and contact the people who represent you at the local, county, state, and federal levels of government.</p>
             </div>
             <div>
-            <p className='aboutContainer2'>Talking and engaging with your representatives is one of the best ways to effect change in your local community, so reach out to them today with your concerns and <b>let your voice be heard!</b></p>
+            <p className='aboutContainer'>Talking and engaging with your representatives is one of the best ways to effect change in your local community, so reach out to them today with your concerns and <b>let your voice be heard!</b></p>
             </div>
             <div className='footerContainerAbout'>
                 <Footer2 />
