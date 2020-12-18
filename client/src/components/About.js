0f472072f9bfcaeb27628256fa1622f4e2d23849
wfoody <foodyw@gmail.com>
2020-12-17 19:59:31 -0500

    footer position

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ import { NavLink } from 'react-router-dom'
 function About() {
 
     return (
-        <div>
+        <div className='aboutBodyContainer'>
             <div className='aboutLogoDiv'>
                 <NavLink to='/'><img src={logo} className='aboutLogo' alt='American flag' /></NavLink>
             </div>
@@ -17,12 +17,12 @@ function About() {
                 <p className='aboutContainer'><i><b>REPRESENT</b></i>&nbsp; is a platform designed to connect you with your elected representatives. Using the <a href='https://developers.google.com/civic-information' target="_blank" rel="noopener noreferrer">Google Civic Information API</a> with any U.S. residential address, you have the ability to find and contact the people who represent you at the local, county, state, and federal levels of government.</p>
             </div>
             <div>
-            <p className='aboutContainer'>Talking and engaging with your representatives is one of the best ways to effect change in your local community, so reach out to them today with your concerns and <b>let your voice be heard!</b></p>
+                <p className='aboutContainer'>Talking and engaging with your representatives is one of the best ways to effect change in your local community, so reach out to them today with your concerns and <b>let your voice be heard!</b></p>
             </div>
             <div className='footerContainerAbout'>
-            <p>© 2020 / / REPRESENT</p>
-            <p><NavLink to='/' className='about'>HOME</NavLink></p>
-        </div>
+                <p>© 2020 / / REPRESENT</p>
+                <p><NavLink to='/' className='about'>HOME</NavLink></p>
+            </div>
         </div>
     )
 }
